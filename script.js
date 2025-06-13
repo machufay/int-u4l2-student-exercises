69e723b6c4cae34ec41d55dd9048a9006a1167e6
machufay <mattyangfei@gmail.com>
2025-06-13 14:34:24 -0500

    j

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let captionOne = document.querySelector(".caption-one");
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
-
+let spidyCaption = document.querySelector(".comment-one").value;
 
     // 2. Display the variable spidyCaption to captionOne.
-
+captionOne.innerHTML = spidyCaption;
 
 });
 
 // 3. Repeat the previous steps for the second meme.
 //  - Be sure to store variables for the button and caption.
+let buttonTwo = document.querySelector(".button-two");
+let captionTwo = document.querySelector(".caption-two");
+let kermitCaption = document.querySelector(".comment-two").value;
+captionTwo.innerHTML = kermitCaption;
